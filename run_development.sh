84a931ca61e50d0ca2e0075734b6d9f4033599bd
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2023-06-28 10:25:57 +0200

    🧑‍💻 HTML-endepunkt aktivert lokalt

--- run_development.sh
+++ run_development.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-CURRENT_PATH="$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"
+CURRENT_PATH="$(cd "$(dirname "$1")" || exit; pwd)/$(basename "$1")"
+IMAGE_NAME=ghcr.io/navikt/pdfgen:2.0.9
 
-docker pull ghcr.io/navikt/pdfgen:2.0.9
+docker pull ${IMAGE_NAME}
 docker run \
-        -v $CURRENT_PATH/templates:/app/templates \
-        -v $CURRENT_PATH/fonts:/app/fonts \
-        -v $CURRENT_PATH/data:/app/data \
-        -v $CURRENT_PATH/resources:/app/resources \
+        -v "${CURRENT_PATH}"/templates:/app/templates \
+        -v "${CURRENT_PATH}"/fonts:/app/fonts \
+        -v "${CURRENT_PATH}"/data:/app/data \
+        -v "${CURRENT_PATH}"/resources:/app/resources \
         -p 8080:8080 \
+        -e ENABLE_HTML_ENDPOINT=true \
         -e DISABLE_PDF_GET=false \
         -it \
         --rm \
-        ghcr.io/navikt/pdfgen:2.0.9
+        ${IMAGE_NAME}
